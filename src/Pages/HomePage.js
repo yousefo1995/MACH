42ec7ffeaeff05d99f8ede1c2891e412665fdd16
Yousef Omar <yousefo1995@gmail.com>
2023-09-29 20:39:20 +0300

    NavigatinSection in HomePage was created

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,11 +1,24 @@
-import { Box } from "@mui/material";
+import { Box, Stack, Paper } from "@mui/material";
 import React from "react";
 import HeroSection from "../Components/HomePageComponents/HeroSection";
+import NavigationSection from "../Components/HomePageComponents/NavigationSection";
 
 const HomePage = () => {
   return (
     <Box>
       <HeroSection />
+      {/* Floating NavigationSection */}
+      {/* <Stack width="100%" alignItems="center" marginTop={-6} marginBottom={4}>
+        <Paper
+          sx={{ width: "90%", bgcolor: "white", zIndex: "2", padding: "20px" }}
+          elevation={2}
+        >
+          <NavigationSection />
+        </Paper>
+      </Stack> */}
+      <Stack marginTop={7} marginBottom={7}>
+        <NavigationSection />
+      </Stack>
     </Box>
   );
 };
