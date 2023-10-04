8483e994021598ad0ef62078f4a83c71a0004448
Yousef Omar <yousefo1995@gmail.com>
2023-10-04 18:39:22 +0300

    Add the OurVlaues section to AboutUs Page and update some styles in it

--- src/Pages/AboutUsPage.js
+++ src/Pages/AboutUsPage.js
@@ -2,7 +2,18 @@ import React, { useEffect } from "react";
 import { Typography, Container, Stack } from "@mui/material";
 import logo from "../Components/images/LogoCropped.png";
 import aboutUsBackround from "../Components/images/aboutUsBackground.jpg";
-// import OurValues from "../Components/AboutUsPageComponents/OurValues";
+import OurValues from "../Components/AboutUsPageComponents/OurValues";
+
+const textData = {
+  enTitle: "Who Are We?",
+  enParagraph:
+    " Mishal Abdulmohsin Alhokair LTD. (MAHC) has been active in trading and IT services since 2018, with significant partners all over the kingdom. With a team that has valuable experience deploying innovative ideas and intelligent strategies, we enable our clients to accelerate their business growth with excellent convenience and impressive tech solutions.",
+  arTitle: "عن",
+  arParagraph1:
+    "ﺷــــــﺮﻛﺔ ﻣﺸﻌﻞ ﻋﺒـــﺪ اﻟﻤﺤﺴــــﻦ اﻟﺤــــﻜﻴﺮ اﻟﻤـــﺤﺪودة ، ﻫـــﻲ ﺷــــﺮﻛﺔ ﻧﺸﻄــــﺔ ﻓــــﻲ اﻟﺘﺠﺎرة وﺧـــــﺪﻣﺎت ﺗﻜﻨـــــــﻮﻟﻮﺟـــﻴﺎ اﻟﻤﻌـــــﻠﻮﻣﺎت ﻣﻨﺬ ﻋﺎم2018، ﻣﺘﻌـــﺎوﻧﺔ ﻣﻊ ﺷــﺮﻛﺎء ﻣﻬــﻤﻴﻦ ﻓـــﻲ ﺟـــﻤﻴﻊ أﻧﺤـــﺎء اﻟﻤﻤـــﻠﻜﺔ. ",
+  arParagraph2:
+    "ﻣﻦ ﺧﻼل ﻓﺮﻳﻖ ﻳﺘﻤﺘﻊ ﺑﺨﺒﺮة راﺳﺨﺔ ﻓﻲ ﺗﻨﻔﻴﺬ اﻷﻓﻜﺎر اﻟﻤﺒﺘﻜﺮة وﺻﻨﺎﻋﺔ اﻻﺳﺘﺮاﺗﻴﺠﻴﺎت، ﻧﺴﺘﻄﻴﻊ ﺗﻤﻜﻴﻦ ﻋﻤﻼﺋﻨﺎ ﻣﻦ ﺗﺴﺮﻳﻊ ﻧﻤﻮ أﻋﻤﺎﻟﻬﻢ ﺑﺄﻋﻠﻰ ﻣﺴﺘﻮى ﻣﻦ اﻟﺮاﺣﺔ وﻋﻦ ﻃﺮﻳﻖ ﺣﻠﻮل ﺗﻘﻨﻴﺔ ذﻛﻴﺔ.",
+};
 
 const AboutUsPage = () => {
   useEffect(() => {
@@ -10,107 +21,67 @@ const AboutUsPage = () => {
   }, []);
 
   return (
-    <Stack
-      sx={{
-        backgroundImage: `url(${aboutUsBackround})`,
-        backgroundSize: "cover",
-        minHeight: "85vh",
-      }}
-    >
+    <Stack>
       <Container
-      // sx={{
-      //   width: "100%",
-      //   alignItems: "center",
-      // }}
+        maxWidth="xl"
+        // sx={{
+        //   width: "100%",
+        //   alignItems: "center",
+        // }}
       >
         <Stack
-          color="primary.main"
-          alignItems="center"
-          px={{ xs: 4, md: 22 }}
-          my={8}
+          flexDirection={{ xs: "column-reverse", lg: "row" }}
+          justifyContent="space-between"
+          marginY={{ xs: 4, sm: 9 }}
         >
-          {/* <Paper elevation={3}> */}
-          {/* <img src={logo} alt="Company logo" style={{ width: "200px" }} /> */}
-          <Typography variant="h1" gutterBottom>
-            Who Are We?
-          </Typography>
-          <Typography variant="body1" align="center">
-            Mishal Abdulmohsin Alhokair LTD. (MAHC) has been active in trading
-            and IT services since 2018, with significant partners all over the
-            kingdom. With a team that has valuable experience deploying
-            innovative ideas and intelligent strategies, we enable our clients
-            to accelerate their business growth with excellent convenience and
-            impressive tech solutions.
-          </Typography>
-          <Stack flexDirection="row" mt={4} alignItems="center">
-            <img src={logo} alt="Company logo" style={{ width: "140px" }} />
-            <Typography variant="h1" gutterBottom pt={1} ml={0.5}>
-              عن{" "}
-            </Typography>
+          <Stack
+            color="primary.main"
+            // alignItems="center"
+            width={{ xs: "100%", lg: "45%" }}
+            // px={{ xs: 4, md: 22 }}
+            mx={2}
+            justifyContent="space-evenly"
+          >
+            <Stack>
+              <Typography variant="h1" mb={2}>
+                {textData.enTitle}
+              </Typography>
+              <Typography variant="body1">{textData.enParagraph}</Typography>
+            </Stack>
+            <Stack>
+              <Stack
+                flexDirection="row"
+                mt={4}
+                mb={1}
+                alignItems="center"
+                alignSelf="flex-end"
+              >
+                <img src={logo} alt="Company logo" style={{ width: "140px" }} />
+                <Typography variant="h1" gutterBottom pt={1} ml={0.5}>
+                  {textData.arTitle}
+                </Typography>
+              </Stack>
+              <Typography variant="body1" dir="rtl" align="right">
+                {textData.arParagraph1}
+              </Typography>
+              <Typography variant="body1" dir="rtl" align="right">
+                {textData.arParagraph2}
+              </Typography>
+            </Stack>
+            {/* </Paper> */}
           </Stack>
-          <Typography variant="body1" align="center">
-            (MAHC) شركة مشعل عبد المحسن الحكير المحدودة (ماحك) هي شركة نشطة في
-            التجارة وخدمات تكنولوجيا المعلومات منذ عام 2018، مع شركاء كبار في
-            جميع أنحاء المملكة. بفريق لديه خبرة قيمة في تنفيذ أفكار مبتكرة
-            واستراتيجيات ذكية، نمكن عملائنا من تسريع نمو أعمالهم براحة ممتازة
-            وحلول تكنولوجية رائعة.
-          </Typography>
-          {/* </Paper> */}
-
-          <Stack mt={4}>
-            <Typography variant="h1" gutterBottom>
-              Our Values
-            </Typography>
+          <Stack
+            width={{ xs: "100%", lg: "50%" }}
+            alignItems={{ xs: "center", lg: "flex-end" }}
+            mt={4}
+            mr={5}
+            mb={{ xs: 8, lg: 0 }}
+          >
+            <OurValues />
           </Stack>
         </Stack>
       </Container>
-
-      {/* <OurValues /> */}
     </Stack>
-
-    /*
-    // bushra design
-    <Stack flexDirection="row">
-      <Stack
-        width="40%"
-        bgcolor="#CCCCCC"
-        alignItems="center"
-        // height="400px"
-      >
-        <Typography
-          fontSize="42px"
-          color="primary"
-          fontWeight="700"
-          gutterBottom
-        >
-          Who Are We?{" "}
-        </Typography>
-        <Paper elevation={11} sx={{ width: "80%" }}>
-          <Typography variant="body1" padding={1}>
-            Mishal Abdulmohsin Alhokair LTD. (MAHC) has been active in trading
-            and IT services since 2018, with significant partners all over the
-            kingdom. With a team that has valuable experience deploying
-            innovative ideas and intelligent strategies, we enable our clients
-            to accelerate their business growth with excellent convenience and
-            impressive tech solu
-          </Typography>
-        </Paper>
-      </Stack>
-      <Stack width="60%">
-        <img
-          src={aboutUsImage}
-          alt=""
-          style={{
-            width: "100%",
-            // maxHeight: "460px",
-            height: "500px",
-            objectFit: "cover",
-            objectPosition: "center",
-          }}
-        />
-      </Stack>
-    </Stack>
-    */
   );
 };
 
