60df45fb94b6294b15bf13ab313cf8c79934c608
Yousef Omar <yousefo1995@gmail.com>
2023-10-14 12:52:08 +0300

    Change images and clean code for the products page

---
+++ src/Components/ProductsPageComponents/ProductCard.js
@@ -0,0 +1,26 @@
+import { Paper, Stack, Typography } from "@mui/material";
+import React from "react";
+
+const ProductCard = ({ name, img, enName, arName }) => {
+  return (
+    <Paper elevation={10} sx={{ width: "280px", bgcolor: "primary.main" }}>
+      <Stack alignItems="center" pb={2} color="#fff">
+        <img
+          src={img}
+          alt={name}
+          width="100%"
+          height="280px"
+          style={{ objectFit: "cover" }}
+        />
+        <Typography my={2} variant="h2">
+          {enName}
+        </Typography>
+        <Typography variant="h2" dir="rtl">
+          {arName}
+        </Typography>
+      </Stack>
+    </Paper>
+  );
+};
+
+export default ProductCard;
