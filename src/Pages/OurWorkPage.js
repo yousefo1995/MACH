13f9883f6a52107d3cb02d05c91816cf7d1b7dd2
Yousef Omar <yousefo1995@gmail.com>
2023-10-23 15:30:52 +0300

    Change slider and grid data

--- src/Pages/OurWorkPage.js
+++ src/Pages/OurWorkPage.js
@@ -1,30 +1,101 @@
-import { Container, Stack, Typography, Box, Paper, Grid } from "@mui/material";
+import { Container, Stack, Typography, Box } from "@mui/material";
 import React, { useState } from "react";
-import basicImage from "../Components/images/OurWorkBg.png";
-import image2 from "../Components/images/Service.jpg";
-import image3 from "../Components/images/contachUsCover (1).jpg";
-import image4 from "../Components/images/service4.jpg";
-import image5 from "../Components/images/vision2.jpg";
-import image6 from "../Components/images/products/DigitalSignage1.jpg";
 
 import GridImage from "../Components/OurWorkPageComponents/GridImage";
 import SimpleSlider from "../Components/Core/Slider/SimpleSlider";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 
+const gridImage = [
+  {
+    img1: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FGrid%2FInterior%20LED%20img1.jpg?alt=media&token=e26b473d-4caa-4d8a-b682-a79b85c2a530",
+    img2: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FGrid%2FInterior%20LED%20img2.jpeg?alt=media&token=7b568011-a63a-4d15-ad75-3527aabb9951",
+    // arTitle: "الشاشات الداخلية",
+    enTitle: "Interior LED Screens",
+    isLeft: false,
+  },
+  {
+    img1: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FGrid%2FInterior%20LED%20img1.jpg?alt=media&token=e26b473d-4caa-4d8a-b682-a79b85c2a530",
+    img2: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FGrid%2FInterior%20LED%20img2%20a.jpg?alt=media&token=4d98569a-b06c-4530-a026-d26cc77f2b6a", // arTitle: "الشاشات الداخلية",
+    enTitle: "Interior LED Screens",
+    isLeft: false,
+  },
+  {
+    img1: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FGrid%2Fgym%20img1.jpg?alt=media&token=3db6e3b9-26e0-4816-9de3-0d7b5d3e2af9",
+    img2: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FGrid%2Fgym%20img2.jpg?alt=media&token=b768c17d-75de-4f55-91cb-25409c741a6e",
+    // arTitle: "شاشات مخصصة للأندية الرياضية",
+    enTitle: "Specialized Screens for Gyms",
+    isLeft: true,
+  },
+  {
+    img1: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FGrid%2FSPARKYS%20img1%20a.jpeg?alt=media&token=e2790234-02af-4dd9-bc69-7c2b2e8866d1",
+    img2: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FGrid%2FSPARKYS%20img2.jpg?alt=media&token=7027d792-efb6-4e54-a99e-9c6e53d1d8e3",
+    // arTitle: "",
+    enTitle: "SPARKYS",
+    isLeft: false,
+  },
+];
 const images = [
-  { url: basicImage, title: "ourWork" },
-  { url: image2, title: "ourWork" },
-  { url: image3, title: "ourWork" },
-  { url: image4, title: "ourWork" },
-  { url: image5, title: "ourWork" },
-  { url: image6, title: "ourWork" },
-  { url: image2, title: "ourWork" },
-  { url: basicImage, title: "ourWork" },
-  { url: image3, title: "ourWork" },
-  { url: image4, title: "ourWork" },
-  { url: basicImage, title: "ourWork" },
-  { url: image3, title: "ourWork" },
+  {
+    url: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FSlider%2Fslider%201.jpeg?alt=media&token=c21057ca-967a-4f3f-b750-b7c94ea14409",
+    title: "ourWork1",
+  },
+  {
+    url: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FSlider%2Fslider%202.jpeg?alt=media&token=36c96119-4bf6-4351-8c9e-03bd02838184",
+    title: "ourWork2",
+  },
+  {
+    url: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FSlider%2Fslider%203.jpg?alt=media&token=d9d472af-b22a-4764-93bb-68b3178cdc66",
+    title: "ourWork3",
+  },
+  {
+    url: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FSlider%2Fslider%204.jpg?alt=media&token=284543af-cbf4-42ce-83b0-9e22d9f7aa6a",
+    title: "ourWork4",
+  },
+  {
+    url: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FSlider%2Fslider%205.jpg?alt=media&token=de5d19f0-4e82-4692-96a7-c14e0edb3951",
+    title: "ourWork5",
+  },
+  {
+    url: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FSlider%2Fslider%206.jpg?alt=media&token=d9fe7396-3293-437f-b180-850ae54d702b",
+    title: "ourWork6",
+  },
+  {
+    url: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FSlider%2Fslider%207.jpg?alt=media&token=8ce445ae-c2da-4db8-9c54-0b376fcad3bb",
+    title: "ourWork7",
+  },
+  {
+    url: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FSlider%2Fslider%208.jpeg?alt=media&token=3fbf8e44-9d1f-419b-93ef-b84422e69a7f",
+    title: "ourWork8",
+  },
+  {
+    url: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FSlider%2Fslider%209.jpeg?alt=media&token=8673dfa1-6fa2-402b-b9c8-b7b3723b08d0",
+    title: "ourWork9",
+  },
+  {
+    url: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FSlider%2Fslider%2010.jpeg?alt=media&token=d8f22a3a-0a6f-460c-923c-ad5de57537ed",
+    title: "ourWork10",
+  },
+  {
+    url: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FSlider%2Fslider%2011.jpg?alt=media&token=0840db5b-bc3e-40b0-813e-f242af75f676",
+    title: "ourWork11",
+  },
+  {
+    url: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FSlider%2Fslider%2012.jpg?alt=media&token=f7d39f57-4051-40ea-8961-cbd440a341b7",
+    title: "ourWork12",
+  },
+  {
+    url: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FSlider%2Fslider%2013.jpg?alt=media&token=94c14d77-7c0f-445b-800e-5e10b6ac533d",
+    title: "ourWork13",
+  },
+  {
+    url: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FSlider%2Fslider%2014.jpg?alt=media&token=5d7d240d-778e-455e-9ee1-dfa370cedf05",
+    title: "ourWork14",
+  },
+  {
+    url: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FSlider%2Fslider%2015.png?alt=media&token=3461ee80-9753-4772-8663-932c3b1101f1",
+    title: "ourWork15",
+  },
 ];
 
 const ImageCard = ({ img, title, onClick }) => {
@@ -48,7 +119,7 @@ const ImageCard = ({ img, title, onClick }) => {
 
 const OurWorkPage = () => {
   const [bigImage, setBigImage] = useState({
-    url: basicImage,
+    url: "https://firebasestorage.googleapis.com/v0/b/mahc-7c309.appspot.com/o/OurWork%2FSlider%2Fslider%201.jpeg?alt=media&token=c21057ca-967a-4f3f-b750-b7c94ea14409",
     title: "basicImage",
   });
 
@@ -67,7 +138,7 @@ const OurWorkPage = () => {
             </span>
           </Typography>
           <Typography
-            color="primary.main"
+            color="secondary.dark"
             textAlign="center"
             mt={4}
             mb={10}
@@ -78,26 +149,16 @@ const OurWorkPage = () => {
             industries.
           </Typography>
 
-          <GridImage
-            img1={basicImage}
-            img2={image3}
-            arTitle="الشاشات الداخلية"
-            enTitle="Interior LED Screens"
-          />
-          <GridImage
-            img1={basicImage}
-            img2={image3}
-            isLeft={true}
-            arTitle="الشاشات الداخلية"
-            enTitle="Interior LED Screens"
-          />
-          <GridImage
-            img1={basicImage}
-            img2={image3}
-            arTitle="الشاشات الداخلية"
-            enTitle="Interior LED Screens"
-          />
-
+          {gridImage.map((item, index) => (
+            <GridImage
+              key={index}
+              img1={item.img1}
+              img2={item.img2}
+              arTitle={item.arTitle}
+              enTitle={item.enTitle}
+              isLeft={item.isLeft}
+            />
+          ))}
           <Stack mb={4} width="90%" display={{ xs: "none", sm: "flex" }}>
             <Card
               elevation={10}
